56571d9700d63bbbcec3899a025aed051571f953
Juan Francisco Perez <juanf.perez.97@gmail.com>
2021-12-09 21:48:27 -0300

    animacion flip

--- src/components/atoms/Card.tsx
+++ src/components/atoms/Card.tsx
@@ -8,21 +8,38 @@ interface CardProps {
 
 const Card = (props: CardProps) => {
   return (
-    <div
-      className={
-        props.covered ? 'cardContainerCovered' : 'cardContainerUncovered'
-      }
-      onClick={props.handleClick}
-    >
-      {props.covered ? (
-        <img
-          className='cardContent'
-          src='https://upload.wikimedia.org/wikipedia/commons/6/61/Flag.svg'
-          alt='asd'
-        />
-      ) : (
-        <img className='cardContent' src={props.src} alt='asd' />
-      )}
+    <div className={`card ${props.covered ? '' : 'active'}`}>
+      <div className='cardWrapper'>
+        <div className={'cardContainerCovered'} onClick={props.handleClick}>
+          {
+            <img
+              className='cardContent'
+              src='https://upload.wikimedia.org/wikipedia/commons/6/61/Flag.svg'
+              alt='asd'
+            />
+          }
+        </div>
+        <div className={'cardContainerUncovered'} onClick={props.handleClick}>
+          {<img className='cardContent' src={props.src} alt='asd' />}
+        </div>
+      </div>
+
+      {/* <div
+        className={
+          props.covered ? 'cardContainerCovered' : 'cardContainerUncovered'
+        }
+        onClick={props.handleClick}
+      >
+        {props.covered ? (
+          <img
+            className='cardContent'
+            src='https://upload.wikimedia.org/wikipedia/commons/6/61/Flag.svg'
+            alt='asd'
+          />
+        ) : (
+          <img className='cardContent' src={props.src} alt='asd' />
+        )}
+      </div> */}
     </div>
   )
 }
